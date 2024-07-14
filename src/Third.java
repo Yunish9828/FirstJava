d3b0c1d253285420bd1a3c70f51aa1395ed967af
[yunistha2019@gmail.com] <yunistha2019@gmail.com>
2024-07-14 23:05:41 +0545

    third

--- src/Third.java
+++ src/Third.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 public class Third {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
         System.out.println("Enter your age: ");
-        int age = scanner.nextInt();
-        if (age >= 18) {
-            System.out.println("You can vote.");
+        int age = sc.nextInt();
+        System.out.println("Enter your nationality: ");
+        String nationality = sc.next();
+        if (age >= 18 && nationality.equals("Nepali")) {
+            System.out.println("You can vote");
         }
         else{
             System.out.println("You cannot vote.");
